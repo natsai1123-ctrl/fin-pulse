221060e27f61bf2d26370948479fd7b3d87d4f6f
natsai1123-ctrl <natsai1123@gmail.com>
2026-09-17 13:25:03 +0800

    refactor: revamp app with standard react 19 and secure firebase real-time dashboard

---
+++ firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDrGC_b-M7-VeqDhzsBY7JvP8mznMxBIII",
+  authDomain: "://firebaseapp.com",
+  projectId: "fin-pulse-adf61",
+  storageBucket: "fin-pulse-adf61.firebasestorage.app",
+  messagingSenderId: "960592027576",
+  appId: "1:960592027576:web:b3a048b8b110054d1cb94d",
+  measurementId: "G-7T2VZHHMGF"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+
+export default app;
