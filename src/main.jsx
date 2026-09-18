fccaf7395a0122cc0c75865e8979a9fc93de9ed5
natsai1123-ctrl <natsai1123@gmail.com>
2026-09-18 10:20:31 +0800

    fix: re-route main entry to App.jsx and completely clear backup ghosts

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx' // 💡 校正點：確保讀取的是昨天完工的 App.jsx
 import './index.css'
-import App from './FinPulse.jsx' // 
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>,
 )
