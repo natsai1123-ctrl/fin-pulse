0428b914f97612adbe1a800af2650012b733a36e
natsai1123-ctrl <natsai1123@gmail.com>
2026-09-18 10:22:23 +0800

    fix: pristine app system alignment

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,1 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx' // 💡 校正點：確保讀取的是昨天完工的 App.jsx
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+﻿import React from "react"`nimport { StrictMode } from "react"`nimport { createRoot } from "react-dom/client"`nimport "./index.css"`nimport App from "./App.jsx"`n`ncreateRoot(document.getElementById("root")).render(`n  <StrictMode>`n    <App />`n  </StrictMode>,`n)
